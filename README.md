4c58478ec189f41d7f2dae73aa9b301d6a9b7f2d
ram-019 <160409368+ram-019@users.noreply.github.com>
2024-02-19 10:14:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-# Robbery-Detection-System
+# BigBrother
 
 ## Gun Detection
+
 We use YOLOv5 to detect guns in the video. Since a dataset that meets our needs was not found, few frames of videos of CCTV footage of gun point robbery were annotated. Since the size of dataset was not large enough, model performs only fairly accurately. This can be improved by training it on more number of annotated images.
 
 ## Action detection/trigger
-1 indicates no activity of interest. 0 indicates an activity of interest is occuring in the video.
+
+1 indicates no activity of interest.
+0 indicates an activity of interest is occuring in the video.
 
 Currently, the action it detects is hostages raising their hands. This can be changed by training the model to detect other actions.
---
+++ movenet.py
@@ -0,0 +1,90 @@
+from statistics import mode
+import tensorflow as tf
+import tensorflow_hub as hub
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+#model=hub.load('https://tfhub.dev/google/movenet/multipose/lightning/1')
+linux_path='/home/shyams/Projects/BigBrother/movenet_multipose_lightning_1'
+windows_path='C:\Files\Academics\VIT\Project\BigBrother\multipose_lightning_1'
+model_path=''
+if os.path.exists(linux_path):
+    model_path=linux_path
+elif os.path.exists(windows_path):
+    model_path=windows_path
+
+model=hub.load(model_path)
+
+movenet=model.signatures['serving_default']
+
+
+def draw_keypoints(frame,keypoints,confidence_threshold):
+    y,x,c=frame.shape
+    shaped=np.squeeze(np.multiply(keypoints,[y,x,1]))
+    
+    for kp in shaped:
+        ky,kx,kp_conf=kp
+        if kp_conf>confidence_threshold:
+            cv2.circle(frame,(int(kx),int(ky)),4,(0,255,0),-1)
+
+EDGES = {
+    (0, 1): 'm',
+    (0, 2): 'c',
+    (1, 3): 'm',
+    (2, 4): 'c',
+    (0, 5): 'm',
+    (0, 6): 'c',
+    (5, 7): 'm',
+    (7, 9): 'm',
+    (6, 8): 'c',
+    (8, 10): 'c',
+    (5, 6): 'y',
+    (5, 11): 'm',
+    (6, 12): 'c',
+    (11, 12): 'y',
+    (11, 13): 'm',
+    (13, 15): 'm',
+    (12, 14): 'c',
+    (14, 16): 'c'
+}
+
+def draw_connections(frame, keypoints, edges, confidence_threshold):
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints, [y,x,1]))
+    
+    for edge, color in edges.items():
+        p1, p2 = edge
+        y1, x1, c1 = shaped[p1]
+        y2, x2, c2 = shaped[p2]
+        
+        if (c1 > confidence_threshold) & (c2 > confidence_threshold):      
+            cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,255), 4)
+
+def loop_through_people(frame, keypoints_with_scores, edges, confidence_threshold):
+    for person in keypoints_with_scores:
+        draw_connections(frame, person, edges, confidence_threshold)
+        draw_keypoints(frame, person, confidence_threshold)
+
+cap=cv2.VideoCapture("data/video1.mp4")#-1)
+while cap.isOpened():
+    ret,frame=cap.read()
+    
+    img=frame.copy()
+    img=tf.image.resize_with_pad(tf.expand_dims(img,axis=0),256,256)
+    input_img=tf.cast(img,dtype=tf.int32)
+    
+    results=movenet(input_img)
+    keypoints_with_scores=results['output_0'].numpy()[:,:,:51].reshape(6,17,3)
+    #print(results)
+    
+    loop_through_people(frame,keypoints_with_scores,EDGES,0.3)
+    
+    cv2.imshow('Movenet',frame)
+    
+    if(cv2.waitKey(10) & 0xFF==ord('q')):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
